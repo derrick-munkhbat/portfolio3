527f6ca1f9855228e210e724d00048de20739ded
dzone24 <derrick.munkhbat@gmail.com>
2024-09-18 10:39:47 +0800

    profile stroke added and div alignment fixed

--- app/page.jsx
+++ app/page.jsx
@@ -19,23 +19,22 @@ const Home = () => {
               I'm looking for opportunities to expand my experience and skills
               in full-stack development.
             </p>
-          </div>
-          {/* button and socials */}
-          <div className="flex flex-col xl:flex-row items-center gap-8">
-            <Button
-              variant="outline"
-              size="lg"
-              className="uppercase flex items-center gap-2"
-            >
-              <span>Download CV</span>
-              <FiDownload className="text-xl" />
-            </Button>
-
-            <div className="mb-8 xl:mb-0">
-              <Social
-                containerStyles="flex gap-6"
-                iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
-              />
+            {/* button and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
             </div>
           </div>
           {/* photo */}
