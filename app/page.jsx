3689d0b5a02163db64a351254a0ae0a12f0aca78
dzone24 <derrick.munkhbat@gmail.com>
2024-09-23 10:08:01 +0800

    bio replaced

--- app/page.jsx
+++ app/page.jsx
@@ -17,8 +17,11 @@ const Home = () => {
               <span className="text-accent">Derrick Munkhbat</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-              I'm looking for opportunities to expand my experience and skills
-              in full-stack development.
+              I'm looking to expand my skills and experience in full-stack
+              development, taking on new challenges and staying up-to-date with
+              the latest technologies and trends. I'm excited to explore
+              opportunities that will help me grow as a developer and deliver
+              high-quality solutions.
             </p>
             {/* button and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
