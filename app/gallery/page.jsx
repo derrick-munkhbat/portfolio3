4a035c4a6cd4ddba7e23892cd33c8a562ef8bf97
dzone24 <derrick.munkhbat@gmail.com>
2024-10-07 14:51:52 +0800

    ymr ch baisan image locate hiisen.

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -1,65 +1,10 @@
 "use client";
 
 import Image from "next/image";
-// import ImageGallery from "./ImageGallery";
 
 const images = [
   {
-    image: "/images/image1.jpg",
-    thumbnail: "/images/image1.jpg",
-  },
-  {
-    image: "/images/image2.jpg",
-    thumbnail: "/images/image2.jpg",
-  },
-  {
-    image: "/images/image3.jpg",
-    thumbnail: "/images/image3.jpg",
-  },
-  {
-    image: "/images/image4.jpg",
-    thumbnail: "/images/image4.jpg",
-  },
-  {
-    image: "/images/image5.jpg",
-    thumbnail: "/images/image5.jpg",
-  },
-  {
-    image: "/images/image6.jpg",
-    thumbnail: "/images/image6.jpg",
-  },
-  {
-    image: "/images/image7.jpg",
-    thumbnail: "/images/image7.jpg",
-  },
-  {
-    image: "/images/image8.jpg",
-    thumbnail: "/images/image8.jpg",
-  },
-  {
-    image: "/images/image9.jpg",
-    thumbnail: "/images/image9.jpg",
-  },
-
-  {
-    image: "/images/image10.jpg",
-    thumbnail: "/images/image10.jpg",
-  },
-  {
-    image: "/images/image11.jpg",
-    thumbnail: "/images/image11.jpg",
-  },
-  {
-    image: "/images/image12.jpg",
-    thumbnail: "/images/image12.jpg",
-  },
-  {
-    image: "/images/image13.jpg",
-    thumbnail: "/images/image13.jpg",
-  },
-  {
-    image: "/images/image14.jpg",
-    thumbnail: "/images/image14.jpg",
+    image: "/assets/gallery/images/image1.jpg",
   },
 ];
 
