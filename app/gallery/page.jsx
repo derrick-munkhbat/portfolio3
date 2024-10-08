fece6b688a56206cd4f1ef1696fd8807eaaad31a
dzone24 <derrick.munkhbat@gmail.com>
2024-10-08 11:57:40 +0800

    working on click to view text to appear when hovered

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -72,31 +72,36 @@ const Gallery = () => {
       className="h-full flex flex-col px-4"
     >
       <div className="container mx-auto bg-slate-100 dark:bg-slate-800 rounded-2xl">
-        <div className="flex xl:flex-nowrap flex-wrap overflow-x-auto py-5 gap-5 relative">
-          {images.map((image, index) => (
-            <Image
-              key={index}
-              src={image.image}
-              alt="Image"
-              width={500}
-              height={500}
-              onClick={() => handleImageClick(image.image)}
-              className="border rounded-xl xl:w-[1280px] xl:h-[720px] cursor-pointer hover:opacity-80"
-            />
-          ))}
+        <div className="relative">
+          <div className=" flex flex-wrap xl:flex-nowrap xl:justify-start justify-center overflow-x-auto py-5 gap-5">
+            {images.map((image, index) => (
+              <Image
+                key={index}
+                src={image.image}
+                alt="Image"
+                width={500}
+                height={500}
+                onClick={() => handleImageClick(image.image)}
+                className=" rounded-xl w-full h-full xl:w-[1280px] xl:h-[720px] cursor-pointer hover:opacity-80 "
+              />
+            ))}
+          </div>
+          <h2 className="fixed top-36 left-0 xl:absolute xl:top-10 xl:text-3xl font-bold text-center text-white opacity-50 dark flex justify-center items-center w-full">
+            click to view full screen
+          </h2>
         </div>
       </div>
       {isOpen && (
         <div
-          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center"
+          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center "
           onClick={handleClose}
         >
           <Image
             src={selectedImage}
             alt="Image"
-            width={1920}
-            height={1080}
-            className="max-w-full max-h-full"
+            width={2560}
+            height={1440}
+            className="w-full max-h-full"
           />
         </div>
       )}
